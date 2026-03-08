7527c081014b4bba4aacd29cc51aaddbfe435d46
jfdenboer <jfdenboer@gmail.com>
2026-03-08 17:09:44 +0100

    Update __init__.py

--- spurgeon/services/thumbnail/__init__.py
+++ spurgeon/services/thumbnail/__init__.py
@@ -13,6 +13,7 @@ __all__ = [
     "ThumbnailGenerationError",
     "ThumbnailGenerator",
     "ThumbnailTextGenerationError",
+    "ThumbnailTextGenerator",
 ]
 
 
@@ -20,7 +21,7 @@ def __getattr__(name: str) -> Any:
     if name in {"ThumbnailGenerator", "ThumbnailGenerationError"}:
         module = import_module("spurgeon.services.thumbnail.thumbnail_generator")
         return getattr(module, name)
-    if name == "ThumbnailTextGenerationError":
+    if name in {"ThumbnailTextGenerationError", "ThumbnailTextGenerator"}:
         module = import_module("spurgeon.services.thumbnail.generate_thumbnail_text")
         return getattr(module, name)
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
